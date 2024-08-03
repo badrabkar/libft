bfaa1b9d63d8e8fb7ef9e739f6a9ce7e830c304d
badrabkar <doublequintal@gmail.com>
2024-08-03 08:16:37 +0100

    added description

--- src/ft_memset.c
+++ src/ft_memset.c
@@ -1,17 +1,16 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_memset.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: babkar <marvin@42.fr>                      +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/01 15:11:18 by babkar            #+#    #+#             */
-/*   Updated: 2021/11/19 09:44:25 by babkar           ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+/*
+	Author : badr abkar - nebilx - <doublequintal@gmail.com>
+	Description : ft_memset writes len bytes of c to string b
+*/
+
 #include "../includes/libft.h"
 
 void	*ft_memset(void *b, int c, size_t len)
 {
-	
+    unsigned char   *ptr;
+
+    ptr = (unsigned char*)b;
+    while (len-- > 0)
+        *ptr++ = (unsigned char)c;
+    return b;
 }
